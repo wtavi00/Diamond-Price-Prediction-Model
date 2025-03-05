358f6b9e1af6ee827ef3f9059c74679528ac2154
Avijit Tarafder <avijittarafder22@gmail.com>
2025-03-05 23:02:17 +0600

    Create library.py

---
+++ library.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
