f77c31e650b8a6e088304155adcc95c97e22c1df
Avijit Tarafder <avijittarafder22@gmail.com>
2025-03-10 22:48:24 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,3 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+
+df = pd.read_csv("diamonds.csv")
+
+df = df.drop(columns=['Unnamed: 0'], errors='ignore')  # Drop unnecessary index column if present
