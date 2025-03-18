8826c30f7880a0577ca5322bf2ff5f226bc75b0f
Avijit Tarafder <avijittarafder22@gmail.com>
2025-03-18 11:30:09 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,3 +11,11 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 df = pd.read_csv("diamonds.csv")
 
 df = df.drop(columns=['Unnamed: 0'], errors='ignore')  # Drop unnecessary index column if present
+
+le_cut = LabelEncoder()
+le_color = LabelEncoder()
+le_clarity = LabelEncoder()
+
+df['cut'] = le_cut.fit_transform(df['cut'])
+df['color'] = le_color.fit_transform(df['color'])
+df['clarity'] = le_clarity.fit_transform(df['clarity'])
