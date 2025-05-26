95135d19446263e8653ec8b02f039c4c34ebad0c
Avijit Tarafder <avijittarafder22@gmail.com>
2025-05-26 10:19:13 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -23,3 +23,7 @@ df['clarity'] = le_clarity.fit_transform(df['clarity'])
 X = df.drop(columns=['price'])
 y = df['price']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
