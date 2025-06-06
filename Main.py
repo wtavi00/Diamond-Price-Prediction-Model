db30471aa5842fb4077dbf506f308581b4ca88f7
Avijit Tarafder <avijittarafder22@gmail.com>
2025-06-06 11:55:16 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -46,3 +46,9 @@ mae = mean_absolute_error(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
 r2 = r2_score(y_test, y_pred)
+
+print(f"MAE: {mae}")
+print(f"MSE: {mse}")
+print(f"RMSE: {rmse}")
+print(f"R2 Score: {r2}")
+
