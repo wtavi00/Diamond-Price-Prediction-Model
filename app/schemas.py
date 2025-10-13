4f5b3b0b49711d6f1f2b1f6f0f30d295bfeecfc9
Avijit Tarafder <avijittarafder22@gmail.com>
2025-10-13 06:09:33 +0600

    Create schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class DiamondFeatures(BaseModel):
+    carat: float
+    cut: str
+    color: str
+    clarity: str
+    depth: float
+    table: float
+    x: float
+    y: float
+    z: float
